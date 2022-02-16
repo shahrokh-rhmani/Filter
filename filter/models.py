c6dd265bff285eba12760cbc5050c355730cb7cc
Ali <shahrokh.r1379@gmail.com>
2022-02-16 22:31:41 +0330

    add def __str__

--- filter/models.py
+++ filter/models.py
@@ -4,5 +4,8 @@ from django.db import models
 
 class Product(models.Model):
     name = models.CharField(max_length=100)
-    price = models.DecimalField(max_digits=10, decimal_places=2)
+    price = models.DecimalField(max_digits=10, decimal_places=3)
     quantity = models.IntegerField()
+
+    def __str__(self):
+        return self.name
