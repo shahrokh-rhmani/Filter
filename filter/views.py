28f61cd9c2834854831d7837950b5ee14c3f2cb8
Ali <shahrokh.r1379@gmail.com>
2022-02-16 22:14:09 +0330

    create fbv home

--- filter/views.py
+++ filter/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from .models import Product
 
-# Create your views here.
+
+
+def home(request):
+    products = Product.objects.all()
+
+    context = {
+        'product': products
+    }
+    return render(request, 'home.html', context)
