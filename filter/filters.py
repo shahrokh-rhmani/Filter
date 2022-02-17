ffa01a780eff5162b66305d42e294a9f28799fdd
Ali <shahrokh.r1379@gmail.com>
2022-02-17 22:13:04 +0330

    updates productfilter

--- filter/filters.py
+++ filter/filters.py
@@ -1,1 +1,11 @@
+import django_filters
+from .models import Product
 
+
+
+class ProductFilter(django_filters.FilterSet):
+    quantity = django_filters.AllValuesFilter()
+
+    class Meta:
+        model = Product
+        fields = ['quantity']
