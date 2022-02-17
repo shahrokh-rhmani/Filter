e5631e9b7da7dc879d8a0d692ab94392041bda67
Ali <shahrokh.r1379@gmail.com>
2022-02-17 22:14:14 +0330

    add productfilter to def home

--- filter/views.py
+++ filter/views.py
@@ -1,12 +1,14 @@
 from django.shortcuts import render
 from .models import Product
+from .filters import ProductFilter
 
 
 
 def home(request):
-    products = Product.objects.all()
+    # products = Product.objects.all()
+    product_filter = ProductFilter(request.GET)
 
     context = {
-        'products': products,
+        'product_filter': product_filter,
     }
     return render(request, 'home.html', context)
