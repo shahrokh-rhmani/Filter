bfb053a58baae1bf6de05dc8d1f4351f4a30b25f
Ali <shahrokh.r1379@gmail.com>
2022-02-17 21:33:58 +0330

    add django_filter to installed_apps

--- config/settings.py
+++ config/settings.py
@@ -38,6 +38,10 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
 
+    # third party
+    'django_filter',
+
+
     # local
     'filter.apps.FilterConfig',
 ]
